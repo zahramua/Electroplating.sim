e4681c11f243317c7ad6d58879d22e6fb5d1e00b
zahramua <zahrameow@outlook.com>
2025-12-09 18:54:31 +0300

    Fix formatting in vite.config.ts

--- ElectroPlateSim-2zipzipzip/ElectroPlateSim-2zipzip/ElectroPlateSim-2zip/ElectroPlateSim/vite.config.ts
+++ ElectroPlateSim-2zipzipzip/ElectroPlateSim-2zipzip/ElectroPlateSim-2zip/ElectroPlateSim/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Electroplating.sim/', // REQUIRED for GitHub Pages subpath
+  base: '/Electroplating.sim/' // REQUIRED for GitHub Pages subpath
 })
